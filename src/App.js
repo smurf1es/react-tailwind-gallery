4af533539821968baca60dd70618fbd45342ed10
smurf1es <pahuger_puruhita_27rpl@student.smktelkom-mlg.sch.id>
2020-05-05 15:41:13 +0700

    dummy display with class usage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import React from "react";
-import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Photo by John Doe
+        </div>
+        <ul>
+          <li>
+            <strong>Views:</strong>
+            4000
+          </li>
+          <li>
+            <strong>Downloads:</strong>
+            4000
+          </li>
+          <li>
+            <strong>Likes:</strong>
+            4000
+          </li>
+        </ul>
+      </div>
+      <div className="px-6 py-4">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag1
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag2
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag3
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default App;
