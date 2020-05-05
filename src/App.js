e02078a9ec8de536c0a163c645bf5dbab58e9ff4
smurf1es <pahuger_puruhita_27rpl@student.smktelkom-mlg.sch.id>
2020-05-05 16:30:36 +0700

    where image is not found display not found h1

--- src/App.js
+++ src/App.js
@@ -23,6 +23,9 @@ function App() {
   return (
     <div className="container mx-auto">
       <ImageSearch searchText={(text) => setTerm(text)} />
+      {!isLoading && images.length === 0 && (
+        <h1 className="text-6xl text-center mx-auto mt-32">No images found</h1>
+      )}
       {isLoading ? (
         <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
       ) : (
