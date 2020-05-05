332b6c728730aaf558fb1abba5c3f29cc936251e
smurf1es <pahuger_puruhita_27rpl@student.smktelkom-mlg.sch.id>
2020-05-05 16:27:36 +0700

    text props is passed to the App.js

--- src/App.js
+++ src/App.js
@@ -18,11 +18,11 @@ function App() {
         setIsLoading(false);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [term]);
 
   return (
     <div className="container mx-auto">
-      <ImageSearch />
+      <ImageSearch searchText={(text) => setTerm(text)} />
       {isLoading ? (
         <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
       ) : (
